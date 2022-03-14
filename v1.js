1dd77d51a84cc87e8d7c4590ce0db8fae0675cd6
notanewbie <computerchipcity@aol.com>
2022-03-14 16:30:34 +0000

    Update v1.js

--- v1.js
+++ v1.js
@@ -6,7 +6,8 @@ waiting = true;
   fdata = Fdata
   var xhttp = new XMLHttpRequest();
   z = 0
-  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/" + fdata, false);
+  //xhttp.open("GET", "https://cors-anywhere.herokuapp.com/" + fdata, false);
+  xhttp.open("GET", fdata, false);
   xhttp.setRequestHeader('X-Requested-With','XMLHttpRequest');
   xhttp.send();
   //alert(z)
